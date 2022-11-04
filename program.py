7d07580e4110ffe3a3455e0ca4e365b1a26c0dd5
Bernardo Silva <116351467+gestama04@users.noreply.github.com>
2022-11-04 11:02:08 +0000

    Add files via upload

---
+++ program.py
@@ -0,0 +1,12 @@
+# input a float value for variable a here
+
+
+a=float(input("introduza o número para a?"))
+print("Introduziu para a:", a)
+
+# input a float value for variable b here
+
+# output the result of addition here
+
+print("That's all, folks!")
+
